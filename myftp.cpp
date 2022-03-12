1f68c871ed173ab93240bc82bf920aeaacc663ae
Ryan Schuster <rjs48792@uga.edu>
2022-03-11 19:57:00 -0500

    y

--- myftp.cpp
+++ myftp.cpp
@@ -12,7 +12,9 @@ using namespace std;
 
 void worker(string msg, int sock) {
   int valread;
+  bool guard = true;
   char buffer[1024] = {0};
+  char idBuffer[100] = {0}; //initizales all to null
   string token = msg.substr(0, 4);
   string buf(msg); //constructor casts char[] into string                                                                                   
   int index = buf.find(" "); //index where first space is                                                                                   
@@ -20,10 +22,10 @@ void worker(string msg, int sock) {
   string token2 = buf.substr(index + 1, index2 - index);
   fstream file;
 
-  cout << token;
+  //cout << token;
   if (token.compare("quit") == 0) {
     send(sock , msg.c_str() , msg.length() , 0 );
-    cout << "GOODBYE" << endl;
+    cout << "\nGOODBYE" << endl;
     exit(EXIT_SUCCESS);
   } else if(token.substr(0,3).compare("put")==0) {
      cout << "PUTTING" << endl;
@@ -47,27 +49,73 @@ void worker(string msg, int sock) {
   } else {
     send(sock , msg.c_str() , msg.length() , 0 );
   }
-  valread = read( sock , buffer, 1024);
-  if (token.substr(0,3).compare("get")== 0) {
-    cout << "HERE" << endl;
-    string fileName = buf.substr(index + 1, index2 - index);
-    cout << "File name: " << fileName << endl;
-    ofstream outfile(fileName.c_str());
-    outfile.write(buffer, sizeof(buffer));
-    outfile.close();
+  if (token.substr(0,3).compare("get")== 0 || token.substr(0,3).compare("put")== 0) { //prints out put and get command ID
+    valread = read(sock, idBuffer, 100); //server should send a msg if file request exists or not
+    if (valread < 1) {
+      cout << "Socket read error" <<endl;
+    }
+    cout << idBuffer << endl; //says if there is error or no
+    string existmsg(idBuffer);
+    index = existmsg.find(" ");
+    string errorOrExists = existmsg.substr(0,index);
+    if (errorOrExists.compare("[ERROR]") == 0) {
+      guard = false;
+    } else {
+      valread = read(sock, idBuffer, 100);
+      if (valread < 1) {
+        cout << "Socket read error" <<endl;
+      }
+      cout << " = Command ID " << endl;
+      cout << idBuffer;
+    }
   }
-  printf("%s\n",buffer );
+
+  if (guard) { //if guard is false then error occured so don't read from socket for get
+
+    valread = read( sock , buffer, 1024); //read main msg from server
+    if (valread < 1) {
+        cout << "Socket read error" <<endl;
+    }
+
+    if (token.substr(0,3).compare("get")== 0) {
+      cout << "HERE" << endl;
+      string fileName = buf.substr(index, index2 - index);
+      cout << "File name: " << fileName << endl;
+      /*FILE* file = fopen(fileName.c_str(), "w");
+      if (file != NULL) {
+        fwrite(buffer, sizeof(buffer), 1, file);
+      }
+      fclose(file);*/
+      //also change so a while loop keeps reading 1000 bytes at a time, the while loop exits when the last msg is something like "end of file"
+      ofstream outfile(fileName.c_str()); //Code doesn't work???? maybe the buffer recieved is corrupted
+      outfile.write(buffer, sizeof(buffer));
+      outfile.close();
+    }
+    cout << endl;
+    printf("%s\n",buffer );
+  }
 }
+
+
+void terminateWorker(string msg, int term_sock) {
+  int index = msg.find(" ");
+  string termId = msg.substr(index +1, msg.length());
+  send(term_sock , termId.c_str() , termId.length() , 0 );
+}
+
+
 
 int main(int argc, char const *argv[])
 {
-	if (argc < 3) { //checks if less than three arguments
-		printf("Usage: myftp <machine name> <port>\n");
+	if (argc < 4) { //checks if less than three arguments
+		printf("Usage: myftp <machine name> <nport> <tport>\n");
 		return 0;
 	}
 	const char *hostname = argv[1];
-	int port = atoi(argv[2]);
+	int nport = atoi(argv[2]);
+  int tport = atoi(argv[3]);
 	int sock = 0;
+  int term_sock =0;
 	string msg;
 	struct sockaddr_in serv_addr;
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -77,7 +125,7 @@ int main(int argc, char const *argv[])
 	}
 
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(port);
+	serv_addr.sin_port = htons(nport);
 
 	// Convert IPv4 and IPv6 addresses from text to binary form
 	if(inet_pton(AF_INET, hostname, &serv_addr.sin_addr)<=0)
@@ -91,18 +139,54 @@ int main(int argc, char const *argv[])
 		printf("\nConnection Failed \n");
 		return -1;
 	}
+
+	struct sockaddr_in term_serv_addr;
+	if ((term_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) //terminator socket
+	{
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+
+	term_serv_addr.sin_family = AF_INET;
+	term_serv_addr.sin_port = htons(tport);
+
+	// Convert IPv4 and IPv6 addresses from text to binary form
+	if(inet_pton(AF_INET, hostname, &term_serv_addr.sin_addr)<=0)
+	{
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
+	if (connect(term_sock, (struct sockaddr *)&term_serv_addr, sizeof(term_serv_addr)) < 0)
+	{
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+
+
 	thread tid[50];
 	int i = 0;
+  string firstword;
 	while (true) { //loop until quit is called
 		printf("myftp>");
 		getline(cin, msg); //read user input
-		tid[i] = thread(worker, msg, sock);
-		i++;
-		if (i >= 50) {
-			for (i = 0; i < 50; i++) {
-				tid[i].join();
-			}
-			i = 0;
-		}
+    firstword = msg.substr(0, msg.find(" "));
+    if (firstword.compare("terminate") == 0) {
+      tid[i] = thread(terminateWorker, msg, term_sock); 
+    } else {
+      if (msg.back() == '&') { //run command in seperate thread
+        tid[i] = thread(worker, msg, sock);
+        i++;
+        if (i >= 50) {
+          for (i = 0; i < 50; i++) {
+            tid[i].join();
+          }
+          i = 0;
+        }
+      } else {
+        worker(msg, sock);
+      }
+    }
 	}
 }
+
--- myftpserver.cpp
+++ myftpserver.cpp
@@ -10,13 +10,20 @@
 #include <sys/wait.h>
 #include <dirent.h>
 #include <fstream>
+#include <mutex>
 
 using namespace std;
 bool guard = true;
+mutex mtx;
+int idCounter = 0;
+int termId = -1; //id to be terminated
 
 void socketThread(int newSocket) {
 	char buffer[1024] = {0};
     int valread = read(newSocket , buffer, 1024);
+    if (valread < 1) {
+      cout << "Socket read error" <<endl;
+    }
     string buf(buffer); //constructor casts char[] into string
     int index = buf.find(" "); //index where first space is
     int index2 = buf.find(" ", index + 1);
@@ -33,39 +40,54 @@ void socketThread(int newSocket) {
     args[1] = const_cast<char*>(token2.c_str());
     args[2] = NULL;
 
+    //mutex lock to make sure not two threads get same id
+    mtx.lock();
+    int id = idCounter; //critical section
+    idCounter++;
+    mtx.unlock();
+    string cmdId = to_string(id);
+
 	if (token.compare("get") == 0) {
-      file.open(token2, ios::in | ios::binary);
-      if(file.is_open()){
-        cout<<"[LOG] : File is ready to Transmit.\n";
-      }
-      else{
-        cout<<"[ERROR] : File loading failed, Exititng.\n";
-        exit(EXIT_FAILURE);
-      }
-      string contents((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-      cout<<"[LOG] : Transmission Data Size "<<contents.length()<<" Bytes.\n";
-      cout<<"[LOG] : Sending...\n";
-      int bytes_sent = send(newSocket , contents.c_str() , contents.length() ,0);
-      cout<<"[LOG] : Transmitted Data Size "<<bytes_sent<<" Bytes.\n";
-      cout<<"[LOG] : File Transfer Complete.\n";
-		
+        if (access(token2.c_str(), F_OK) != -1) {
+            string exists = "File exists\n";
+            send(newSocket, exists.c_str(), exists.length(),0);
+            send(newSocket, cmdId.c_str(), cmdId.length(), 0); //send command id
+            file.open(token2, ios::in | ios::binary);
+            cout<<"[LOG] : File is ready to Transmit.\n";
+            string contents((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+            //change so every 1000 bytes it will check if id == termId, if it is, stop reading, send msg for other side to delete incomplete file
+            //should reset termId back to -1 after used
+            //the client should check if msg sent is signal to stop
+            cout<<"[LOG] : Transmission Data Size "<<contents.length()<<" Bytes.\n";
+            cout<<"[LOG] : Sending...\n";
+            int bytes_sent = send(newSocket , contents.c_str() , contents.length() ,0);
+            cout<<"[LOG] : Transmitted Data Size "<<bytes_sent<<" Bytes.\n";
+            cout<<"[LOG] : File Transfer Complete.\n";
+        } else {
+            cout<<"[ERROR] : File does not exist\n";
+            string error = "[ERROR] : File does not exist\n";
+            send(newSocket, error.c_str(), error.length(), 0);
+        }
+                
     } else if (token.compare("put") == 0) {
         int index3 = buf.find("*");
         string token3 = buf.substr(index3+1);
         cout << token3 << endl;
-        char buffer2[1024] = {0};
+        //char buffer2[1024] = {0};
         printf("Put received\n");
         cout << "HERE" << endl;
         string fileName = token2;
         cout << "File name: " << fileName << endl;
+        //same deal as get, check every 1000 bytes if id = termId and if so stop reading and cleanup created files
+        //need a way for the client to know to stop sending bytes
         ofstream outfile(fileName.c_str());
         outfile.write(token3.c_str(), sizeof(token3));
         outfile.close();
     } else if (token.compare("delete") == 0) {
-	string stError = "An error occured while trying to delete this file";
+        string stError = "An error occured while trying to delete this file";
 		if(remove(token2.c_str())!=0){
-		send(newSocket, stError.c_str(), stError.length(), 0);
-	}
+            send(newSocket, stError.c_str(), stError.length(), 0);
+        }
 
     } else if (token.compare("ls") == 0) {
         struct dirent *dr;
@@ -104,12 +126,25 @@ void socketThread(int newSocket) {
 }
 
 
-int main(int argc, char const *argv[]) {
-    if (argc < 2) { //checks if there are at least two arguments
-        printf("usage: myftpserver <port>\n");
-        return 0;
+void clientThread(int newSocket) {
+    guard = true;
+    int i = 0;
+    thread tid[10];
+    while (guard) { //continue to accept requests from a connection
+        tid[i] = thread(socketThread, newSocket); //one client able to do multiple cmds
+        i++;
+        if (i >= 10) {
+            for (i=0; i<10; i++) {
+                tid[i].join();
+            }
+            i = 0;
+        }
     }
-    int port = atoi(argv[1]);
+}
+ 
+
+void listenerThread(int nport) {
+
 	int serverFd, newSocket;
 	struct sockaddr_in address;
 	int opt = 1;
@@ -134,7 +169,7 @@ int main(int argc, char const *argv[]) {
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( port );
+    address.sin_port = htons( nport );
     
     // Forcefully attaching socket to the port 8080
     if (bind(serverFd, (struct sockaddr *)&address,
@@ -149,7 +184,8 @@ int main(int argc, char const *argv[]) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    thread tid[50];
+
+    thread ctid[10]; //allow ten client threads to connect at a time
     int i = 0;
     while (true) { //run through for every quit command
 
@@ -159,21 +195,88 @@ int main(int argc, char const *argv[]) {
             perror("accept");
             exit(EXIT_FAILURE);
         }
-        guard = true;
-        while (guard) { //continue to accept requests from a connection
-            tid[i] = thread(socketThread, newSocket); //new thread for every request, calls socketThread function
-            /*    perror("failed to make thread"); //error checking code to implement later
-                exit(EXIT_FAILURE);
-            }*/
-            i++;
-            if (i >= 50) {
-                for (i = 0; i < 50; i++) {
-                    tid[i].join(); //waits for thread to finish
-                }
-                i = 0;
+        ctid[i] = thread(clientThread, newSocket); //multiple client threading code
+        i++;
+        if (i >= 10) {
+            for (i=0; i<10; i++) {
+                ctid[i].join();
             }
-        
+            i = 0;
         }
+    }
+}
+
+void terminatorThread(int tport) {
+	int serverFd, newSocket;
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof(address);
+    // Creating socket file descriptor
+    if ((serverFd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    // optional code that prevents errors such as 'address already in use'
+    // lets socket be bound to an address already in use.
+    if (setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( tport );
+    
+    // Forcefully attaching socket to the port 8080
+    if (bind(serverFd, (struct sockaddr *)&address,
+                                sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(serverFd, 50) < 0)  
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    
+    while (true) {
+
+        if ((newSocket = accept(serverFd, (struct sockaddr *)&address,
+                        (socklen_t*)&addrlen))<0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        char buffer[1024] = {0};
+        int valread = read(newSocket , buffer, 1024);
+        if (valread < 1) {
+            cout << "Socket read error" <<endl;
+        }
+        string buf(buffer); //constructor casts char[] into string
+        int index = buf.find(" "); //index where first space is
+        string token = buf.substr(0, index); //first word, which should be term id
+        termId = stoi(token); //set string to int and then set to termId
+        //terminate process id which should be token and then go back to top of while loop to wait for next connection
+       
           
     }
+}
+
+int main(int argc, char const *argv[]) {
+    if (argc < 3) { //checks if there are at least two arguments
+        printf("usage: myftpserver <nport> <tport>\n");
+        return 0;
+    }
+    int nport = atoi(argv[1]);
+    int tport = atoi(argv[2]);
+    thread listener = thread(listenerThread, nport);
+    thread terminator = thread(terminatorThread, tport);
+    listener.join();
+    terminator.join();
+
+
 }
